f27b604e9b0ca1c607c2a12639497a7297a63d01
malhaouit <abdelmounaim.abdel@gmail.com>
2024-03-24 19:02:36 -0700

    Display lists

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application.
+=> Current file: 7-states_list.py"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the current SQLAlchemy session."""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page with a list of all State objects present in
+    DBStorage."""
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
